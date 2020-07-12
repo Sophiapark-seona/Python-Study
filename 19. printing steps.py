66857f8fb422677bd607761d48c91c8c34fe8172
sophiapark-seona <suna9267@gmail.com>
2020-07-12 22:37:44 +0900

    add

--- 19. printing steps.py
+++ 19. printing steps.py
@@ -24,3 +24,12 @@ for i in range(5):
         else:
             print(' ', end='')
     print()
+
+# 역삼각형 모양으로 별 출력하기
+for i in range(5):
+    for j in range(5):
+        if j < i:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
