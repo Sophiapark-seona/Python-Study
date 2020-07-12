f11cb2271dfea4bde47e8d465861c6b7c1ab9ec6
sophiapark-seona <suna9267@gmail.com>
2020-07-12 18:59:17 +0900

    for

---
+++ 16 for.py
@@ -0,0 +1,21 @@
+for i in range(5, 12):
+    print('Hello, world!', i)
+
+for i in range(0, 10, 2):
+    print('Hello, world!', i)
+
+for i in range(5, 0, -1):
+    print('Hello world!', i)
+
+for i in reversed(range(5)):
+    print('Hello, world!', i)
+
+fruits = ('apple', 'ornage', 'grape')
+for fruit in fruits:
+    print(fruit)
+
+for letter in 'Python':
+    print(letter, end=' ')
+
+for letter in reversed('Python'):
+    print(letter, end=' ')
