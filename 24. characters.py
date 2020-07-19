59167f778ed2bef5c5cf9517add87256819caf4d
sophiapark-seona <suna9267@gmail.com>
2020-07-20 00:25:01 +0900

    ~24.1

---
+++ 24. characters.py
@@ -0,0 +1,47 @@
+#print('Hello, world!'.replace('world', 'Python'))
+
+
+#table = str.maketrans('aeiou', '12345')
+#a = 'apple'.translate(table)
+#print(a)
+
+
+#b = 'apple, pear, grape, pineapple, orange'.split(', ')
+#print(b)
+
+#d = ' '.join(['apple', 'pear', 'grape', 'pineapple', 'orange'])
+#print(d)
+
+#e = 'python'.upper()
+#print(e)
+
+
+#print('     python    '.lstrip())
+#print('     python    '.rstrip())
+#print('     python    '.strip())
+#print(', python.'.lstrip(','))
+#print(', python. '.strip(',.')) #It don't delete '.'
+#print(', python.'.strip(',.')) #It delete '.'
+
+
+#import string
+##print(', python.'.strip(string.punctuation))
+#print(', python.'.strip(string.punctuation + ' '))
+#print(', python.'.strip(string.punctuation).strip())
+
+
+#print('python'.rjust(10))
+#print('python'.center(10))
+
+
+#print('python'.rjust(10).upper())
+
+
+#print('35'.zfill(4))
+#print('3.5'.zfill(6))
+#print('hello'.zfill(10))
+
+#print('apple pineapple'.find('pl'))
+
+#print('apple pineapple'.rfind('pl'))
+
